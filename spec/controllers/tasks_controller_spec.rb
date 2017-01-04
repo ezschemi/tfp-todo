acce0e9a9bcdabf229362b272cb63ad66327f92a
Enrico Zschemisch <enrico@vicampus.com>
2017-01-04 08:39:14 -0500

    items from DB are returned by ID instead of creation/update date

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe TasksController, type: :controller do
         it "should list the tasks in the database" do
             task1 = FactoryGirl.create(:task)
             task2 = FactoryGirl.create(:task)
+            task1.update_attributes(title: "New title")
 
             get :index
 
@@ -12,6 +13,12 @@ RSpec.describe TasksController, type: :controller do
 
             response_value = ActiveSupport::JSON.decode(@response.body)
             expect(response_value.count).to eq(2)
+
+            response_ids = response_value.collect do |task|
+                task["id"]
+            end
+
+            expect(response_ids).to eq([task1.id, task2.id])
         end
     end
 
